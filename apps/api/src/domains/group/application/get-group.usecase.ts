aa5ae6b08cad881ea64cb2fcf9510a36acd963cc
dc-choi <ddagae0805@gmail.com>
2026-01-25 11:54:07 +0900

    fix: 그룹 조회시도 졸업생 필터 제거

--- apps/api/src/domains/group/application/get-group.usecase.ts
+++ apps/api/src/domains/group/application/get-group.usecase.ts
@@ -18,6 +18,7 @@ export class GetGroupUseCase {
                 students: {
                     where: {
                         deletedAt: null,
+                        graduatedAt: null,
                     },
                     orderBy: {
                         societyName: 'asc',
